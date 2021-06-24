814a2c489135857d4d8d68a16bd9e4242d3caf35
Abhiadi12 <abhisekmishra172@gmail.com>
2021-06-25 02:25:09 +0530

    changes in Revolution page

--- src/components/UI/routeComponents/Revolution.js
+++ src/components/UI/routeComponents/Revolution.js
@@ -9,6 +9,13 @@ import { makeStyles } from "@material-ui/core/styles";
 import vision from "../../../assets/vision.svg";
 import technologyAnimation from "../../../animations/technologyAnimation/data.json";
 import consultationIcon from "../../../assets/consultationIcon.svg";
+import mockUp from "../../../assets/mockupIcon.svg";
+import review from "../../../assets/reviewIcon.svg";
+import design from "../../../assets/designIcon.svg";
+import build from "../../../assets/buildIcon.svg";
+import launch from "../../../assets/launchIcon.svg";
+import maintain from "../../../assets/maintainIcon.svg";
+import iterate from "../../../assets/iterateIcon.svg";
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -41,6 +48,32 @@ const useStyles = makeStyles((theme) => ({
       padding: "2em",
     },
   },
+  mockUpContainer: {
+    maxWidth: "25em",
+    padding: "2em",
+    [theme.breakpoints.down("xs")]: {
+      maxWidth: "30em",
+    },
+  },
+  mockUpImg: {
+    width: "600px",
+    marginTop: "40em",
+    marginLeft: "2em",
+    [theme.breakpoints.down("xs")]: {
+      width: "400px",
+    },
+    [theme.breakpoints.down("md")]: {
+      marginTop: "20em",
+    },
+    [theme.breakpoints.down("sm")]: {
+      marginTop: "10em",
+    },
+    "@media (max-width: 400px)": {
+      width: "250px",
+      marginLeft: "1em",
+      marginTop: "5em",
+    },
+  },
   consultationImg: {
     width: "500px",
     height: "300px",
@@ -48,6 +81,10 @@ const useStyles = makeStyles((theme) => ({
     marginLeft: "3em",
     [theme.breakpoints.down("sm")]: {
       width: "400px",
+    },
+    "@media (max-width: 400px)": {
+      width: "250px",
+      marginLeft: "1em",
     },
   },
 }));
@@ -70,6 +107,7 @@ function Revolution() {
             The Revolution
           </Typography>
         </Grid>
+
         <Grid item container>
           <Grid
             item
@@ -282,6 +320,379 @@ function Revolution() {
               alt="consultationIcon"
               className={classes.consultationImg}
             />
+          </Grid>
+        </Grid>
+        {/* last row */}
+        <Grid item container direction="column">
+          <Grid
+            item
+            container
+            style={{
+              backgroundColor: "#9c27b0",
+            }}
+          >
+            <Grid
+              item
+              container
+              direction="column"
+              className={classes.mockUpContainer}
+            >
+              <Grid item>
+                <Typography variant="h4" color="secondary">
+                  Mock Up
+                </Typography>
+              </Grid>
+              <Grid item>
+                <Typography variant="subtitle1" style={{ color: "white" }}>
+                  Our process begins the moment you realize you need a piece of
+                  technology for your business. Whether you already have an idea
+                  for where to start and what to do, or if you just know you
+                  want to step things up, our initial consultation will help you
+                  examine your business holistically to find the best solutions.
+                </Typography>
+              </Grid>
+              <Grid item>
+                <Typography variant="subtitle1" style={{ color: "white" }}>
+                  Our process begins the moment you realize you need a piece of
+                  technology for your business. Whether you already have an idea
+                  for where to start and what to do, or if you just know you
+                  want to step things up, our initial consultation will help you
+                  examine your business holistically to find the best solutions.
+                </Typography>
+              </Grid>
+              <Grid item>
+                <Typography variant="subtitle1" style={{ color: "white" }}>
+                  Our process begins the moment you realize you need a piece of
+                  technology for your business. Whether you already have an idea
+                  for where to start and what to do, or if you just know you
+                  want to step things up, our initial consultation will help you
+                  examine your business holistically to find the best solutions.
+                </Typography>
+              </Grid>
+            </Grid>
+            <Grid item>
+              <img src={mockUp} alt="mockup" className={classes.mockUpImg} />
+            </Grid>
+          </Grid>
+
+          <Grid
+            item
+            container
+            style={{
+              backgroundColor: "#ff5252",
+            }}
+          >
+            <Grid
+              item
+              container
+              direction="column"
+              className={classes.mockUpContainer}
+            >
+              <Grid item>
+                <Typography variant="h4" color="secondary">
+                  Review
+                </Typography>
+              </Grid>
+              <Grid item>
+                <Typography variant="subtitle1" style={{ color: "white" }}>
+                  Our process begins the moment you realize you need a piece of
+                  technology for your business. Whether you already have an idea
+                  for where to start and what to do, or if you just know you
+                  want to step things up, our initial consultation will help you
+                  examine your business holistically to find the best solutions.
+                </Typography>
+              </Grid>
+              <Grid item>
+                <Typography variant="subtitle1" style={{ color: "white" }}>
+                  Our process begins the moment you realize you need a piece of
+                  technology for your business. Whether you already have an idea
+                  for where to start and what to do, or if you just know you
+                  want to step things up, our initial consultation will help you
+                  examine your business holistically to find the best solutions.
+                </Typography>
+              </Grid>
+              <Grid item>
+                <Typography variant="subtitle1" style={{ color: "white" }}>
+                  Our process begins the moment you realize you need a piece of
+                  technology for your business. Whether you already have an idea
+                  for where to start and what to do, or if you just know you
+                  want to step things up, our initial consultation will help you
+                  examine your business holistically to find the best solutions.
+                </Typography>
+              </Grid>
+            </Grid>
+            <Grid item>
+              <img
+                src={review}
+                alt="review"
+                style={{ height: "600px" }}
+                className={classes.mockUpImg}
+              />
+            </Grid>
+          </Grid>
+
+          <Grid
+            item
+            container
+            style={{
+              backgroundColor: "#26a69a",
+              border: "1px solid red",
+            }}
+          >
+            <Grid
+              item
+              container
+              direction="column"
+              className={classes.mockUpContainer}
+            >
+              <Grid item>
+                <Typography variant="h4" color="secondary">
+                  Design
+                </Typography>
+              </Grid>
+              <Grid item>
+                <Typography variant="subtitle1" style={{ color: "white" }}>
+                  Our process begins the moment you realize you need a piece of
+                  technology for your business. Whether you already have an idea
+                  for where to start and what to do, or if you just know you
+                  want to step things up, our initial consultation will help you
+                  examine your business holistically to find the best solutions.
+                </Typography>
+              </Grid>
+              <Grid item>
+                <Typography variant="subtitle1" style={{ color: "white" }}>
+                  Our process begins the moment you realize you need a piece of
+                  technology for your business. Whether you already have an idea
+                  for where to start and what to do, or if you just know you
+                  want to step things up, our initial consultation will help you
+                  examine your business holistically to find the best solutions.
+                </Typography>
+              </Grid>
+              <Grid item>
+                <Typography variant="subtitle1" style={{ color: "white" }}>
+                  Our process begins the moment you realize you need a piece of
+                  technology for your business. Whether you already have an idea
+                  for where to start and what to do, or if you just know you
+                  want to step things up, our initial consultation will help you
+                  examine your business holistically to find the best solutions.
+                </Typography>
+              </Grid>
+            </Grid>
+            <Grid item>
+              <img src={design} alt="design" className={classes.mockUpImg} />
+            </Grid>
+          </Grid>
+
+          <Grid
+            item
+            container
+            style={{
+              backgroundColor: "#9ccc65",
+            }}
+          >
+            <Grid
+              item
+              container
+              direction="column"
+              className={classes.mockUpContainer}
+            >
+              <Grid item>
+                <Typography variant="h4" color="secondary">
+                  Build
+                </Typography>
+              </Grid>
+              <Grid item>
+                <Typography variant="subtitle1" style={{ color: "white" }}>
+                  Our process begins the moment you realize you need a piece of
+                  technology for your business. Whether you already have an idea
+                  for where to start and what to do, or if you just know you
+                  want to step things up, our initial consultation will help you
+                  examine your business holistically to find the best solutions.
+                </Typography>
+              </Grid>
+              <Grid item>
+                <Typography variant="subtitle1" style={{ color: "white" }}>
+                  Our process begins the moment you realize you need a piece of
+                  technology for your business. Whether you already have an idea
+                  for where to start and what to do, or if you just know you
+                  want to step things up, our initial consultation will help you
+                  examine your business holistically to find the best solutions.
+                </Typography>
+              </Grid>
+              <Grid item>
+                <Typography variant="subtitle1" style={{ color: "white" }}>
+                  Our process begins the moment you realize you need a piece of
+                  technology for your business. Whether you already have an idea
+                  for where to start and what to do, or if you just know you
+                  want to step things up, our initial consultation will help you
+                  examine your business holistically to find the best solutions.
+                </Typography>
+              </Grid>
+            </Grid>
+            <Grid item>
+              <img
+                src={build}
+                alt="build"
+                style={{ height: "600px" }}
+                className={classes.mockUpImg}
+              />
+            </Grid>
+          </Grid>
+
+          <Grid
+            item
+            container
+            style={{
+              backgroundColor: "#795548",
+            }}
+          >
+            <Grid
+              item
+              container
+              direction="column"
+              className={classes.mockUpContainer}
+            >
+              <Grid item>
+                <Typography variant="h4" color="secondary">
+                  Launch
+                </Typography>
+              </Grid>
+              <Grid item>
+                <Typography variant="subtitle1" style={{ color: "white" }}>
+                  Our process begins the moment you realize you need a piece of
+                  technology for your business. Whether you already have an idea
+                  for where to start and what to do, or if you just know you
+                  want to step things up, our initial consultation will help you
+                  examine your business holistically to find the best solutions.
+                  The moment we’ve all been waiting for.
+                </Typography>
+              </Grid>
+              <Grid item>
+                <Typography variant="subtitle1" style={{ color: "white" }}>
+                  When construction comes to a close you’re the first one to
+                  know. We’ll give our final demonstration to show off your
+                  shiny new software in the wild so you know exactly how it will
+                  look to your users.
+                </Typography>
+              </Grid>
+              <Grid item>
+                <Typography variant="subtitle1" style={{ color: "white" }}>
+                  When you say the word, we press the button and launch your
+                  project out to the public. We’re there to ensure everything
+                  goes to plan so you can start reaping the rewards of your
+                  technological investment immediately.
+                </Typography>
+              </Grid>
+            </Grid>
+            <Grid item>
+              <img
+                src={launch}
+                alt="launch"
+                style={{ height: "600px" }}
+                className={classes.mockUpImg}
+              />
+            </Grid>
+          </Grid>
+
+          <Grid
+            item
+            container
+            style={{
+              backgroundColor: "#00bcd4",
+            }}
+          >
+            <Grid
+              item
+              container
+              direction="column"
+              className={classes.mockUpContainer}
+            >
+              <Grid item>
+                <Typography variant="h4" color="secondary">
+                  maintain
+                </Typography>
+              </Grid>
+              <Grid item>
+                <Typography variant="subtitle1" style={{ color: "white" }}>
+                  Our work doesn’t end there.
+                </Typography>
+              </Grid>
+              <Grid item>
+                <Typography variant="subtitle1" style={{ color: "white" }}>
+                  After a successful launch we keep in close contact to listen
+                  to feedback and hear how the project is being received.
+                </Typography>
+              </Grid>
+              <Grid item>
+                <Typography variant="subtitle1" style={{ color: "white" }}>
+                  From there on out we make sure your application is kept up to
+                  date and taking advantage of the best features and practices
+                  available. When new developments arise or new techniques are
+                  discovered in future projects, we will implement those
+                  advancements in your project as part of our routine
+                  maintenance.
+                </Typography>
+              </Grid>
+            </Grid>
+            <Grid item>
+              <img
+                src={maintain}
+                alt="maintain"
+                style={{ height: "600px" }}
+                className={classes.mockUpImg}
+              />
+            </Grid>
+          </Grid>
+
+          <Grid
+            item
+            container
+            style={{
+              backgroundColor: "#ff9e80",
+            }}
+          >
+            <Grid
+              item
+              container
+              direction="column"
+              className={classes.mockUpContainer}
+            >
+              <Grid item>
+                <Typography variant="h4" color="secondary">
+                  Iterate
+                </Typography>
+              </Grid>
+              <Grid item>
+                <Typography variant="subtitle1" style={{ color: "white" }}>
+                  The cycle repeats whenever you come up with a new idea for
+                  extending your current project, or come up with a brand new
+                  system entirely.
+                </Typography>
+              </Grid>
+              <Grid item>
+                <Typography variant="subtitle1" style={{ color: "white" }}>
+                  By planning for future features and changes we can build and
+                  evolve your application over time. As new use cases and
+                  customer needs develop we can respond with continuous
+                  integration of new content.
+                </Typography>
+              </Grid>
+              <Grid item>
+                <Typography variant="subtitle1" style={{ color: "white" }}>
+                  Our iterative process will keep you current and competitive,
+                  allowing you to quickly implement changes instead of waiting
+                  months for a single update.
+                </Typography>
+              </Grid>
+            </Grid>
+            <Grid item>
+              <img
+                src={iterate}
+                alt="iterate"
+                style={{ height: "600px" }}
+                className={classes.mockUpImg}
+              />
+            </Grid>
           </Grid>
         </Grid>
       </Grid>
